ecb5b1ea8411f47e7674b5505a4c55ded5d7268c
Astu04 <legendaryastu@hotmail.com>
2014-04-29 17:58:33 +0200

    Update Sonic_Probe.ino
    Commented out part of the sleepNow function, detailed some little things, added a tutorial I forgot, translate a comment from Spanish to English and explain it more detailed. That part is tricky, be careful if you use this code when you select the IR Pin.

--- Sonic_Probe.ino
+++ Sonic_Probe.ino
@@ -14,12 +14,13 @@
 // https://learn.adafruit.com/tv-b-gone-kit/design-notes
 // https://github.com/shirriff/Arduino-IRremote
 // https://learn.adafruit.com/ir-sensor
+// http://www.righto.com/2009/12/tv-b-gone-for-arduino.html
 //
 //Functions: 
 // 0 - Go to sleep
 // 1 - Torch (Short -> toggle, long -> stay on until no pressed)
 // 2 - Laser (Short -> toggle, long -> stay on until no pressed)
-// 3 - Turn on the class projector (Mitsubishi), 
+// 3 - Turn on/off the class projector (Mitsubishi) and some TVs I use. 
 // 4 - TV-B-Gone
 // 5 - Just sound
 // 6 - Auto go to sleep
@@ -32,8 +33,11 @@
 
 
 #define REPEAT_TX         3 // how many times to transmit the IR remote code
-#define IRLED        9      // LED used to blink when volume too high
-			  // No cambiar, tiene que ser el 9 sí o sí, limitaciones. 
+#define IRLED        9     // LED used to blink
+// This one is tricky. You need to check in ~/sketchbook/libraries/IRremote 
+// for the pin you can use. I, having the pushbutton in Pin3 and having a Pro
+// mini, selected 9. By default, it's 3. 
+
 IRsend irsend; // instantiate IR object
 const int buttonAPin = 2;     // the number of the pushbutton pin
 const int buttonBPin = 3;     // the number of the pushbutton pin
@@ -150,7 +154,7 @@ void irmode()
       unsigned int S_pwr3[36]={4150,500,500,500,500,500,500,500,500,500,1500,500,1500,500,1500,500,1500,500,4100,500,500,500,1500,500,500,500,500,500,500,500,500,500,1500,500,500,500};
     for (int txCount = 0; txCount < REPEAT_TX; txCount++) { // how many times to transmit the IR remote  code
       irsend.sendRaw(S_pwr3,36,38); // Change to match your remote protocol
-    }                              // Syntax: (rawbuf (rawbuf), rawlen (number of entries, frequency (38?))
+    }                              // Syntax: (rawbuf (On/Off timing in usecs, rawlen (number of entries), frequency (38?))
     delay(1000);
 // REMOTE_COde 0x1548008 ? 
 
@@ -187,25 +191,34 @@ void sleepNow()
   pinMode(LASER, LOW);
   pinMode(debug, LOW); 
   
-  delay(1000000);  
+  delay(1000000);  // The code, by now, will die here. This part is
+                   // yet to implement, check the next comment! 
+
+///// After this, I got this part from Ken Shirriff's blog and the 
+//modification he did to the Adafruit's TV-B-Gone. 
+//I don't know how it works, yet, this will last thing I'm going to 
+//do, first I want to make that main functions work. I'm not worried 
+//right now about battery, as I have "2000"mAh (Despite not having 
+//tested them, I'm sure that the batteries are overrated). 
+/////
   
-  set_sleep_mode(buttonAPin);                    // sleep mode is set here
-
-  sleep_enable();                             // enables the sleep bit in the mcucr register
-
-  attachInterrupt(0, wakeUpNow, LOW);         // use interrupt 0 (pin 2) and run function
-  // wakeUpNow when pin 2 gets LOW
-
-  sleep_mode();                               // here the device is actually put to sleep!!
-  // THE PROGRAM CONTINUES FROM HERE ON WAKE
-
-  sleep_disable();                            // first thing after waking, disable sleep
-
-  detachInterrupt(0);                         // disables int 0 as the wakeupnow code will
-                                              // not be executed during normal runtime
+//  set_sleep_mode(buttonAPin);                    // sleep mode is set here
+//
+//  sleep_enable();                             // enables the sleep bit in the mcucr register
+//
+//  attachInterrupt(0, wakeUpNow, LOW);         // use interrupt 0 (pin 2) and run function
+//  // wakeUpNow when pin 2 gets LOW
+//
+//  sleep_mode();                               // here the device is actually put to sleep!!
+//  // THE PROGRAM CONTINUES FROM HERE ON WAKE
+//
+//  sleep_disable();                            // first thing after waking, disable sleep
+//
+//  detachInterrupt(0);                         // disables int 0 as the wakeupnow code will
+//                                              // not be executed during normal runtime
 }
-
-void wakeUpNow()
-{
-  // any needed wakeup code can be placed here
-}
+//
+//void wakeUpNow()
+//{
+//  // any needed wakeup code can be placed here
+//}
